ee978a1ac176a30e54950ace85637aa73b252ad4
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:32:00 -0300

    Add files via upload

---
+++ Uri1145/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uri1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, n, i, soma;
+            i = 1;
+            soma = 0;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            a = int.Parse(vetor[0]);
+
+
+
+            n = int.Parse(vetor[1]);
+
+
+            while (n <= 0)
+            {
+                i = i + 1;
+                vetor[i] = Console.ReadLine();
+                n = int.Parse(vetor[i]);
+            }
+
+            for (i = 0; i <= n - 1; i++)
+            {
+
+                soma += a + i;
+
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
